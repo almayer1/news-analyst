3dbfc717688b276d8ebaa69c423c9f64ba29cbed
Alex Mayer <mayeralex05@gmail.com>
2026-04-05 17:49:46 -0500

    Made settings object in config.py to be imported

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
 from pydantic_settings import BaseSettings
 
-class Settings():
+class Settings(BaseSettings):
     # LLM
     llm_base_url: str = "http://localhost:11434/v1"
     llm_api_key: str = "ollama"
     llm_model: str = "llama3.2"
 
     # Tavily
-    tavily_api_url: str = "placeholder"
+    tavily_api_key: str
+
+    model_config = {"env_file": ".env"}
+
+settings = Settings()
